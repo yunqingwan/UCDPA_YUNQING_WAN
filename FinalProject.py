2e7d27e8d82bba989d10fdb8f07c1c3ff2e8535f
Yunqing Wan <yunqing.wan@gmail.com>
2021-05-10 11:45:20 +0100

    "Fourth Commit"

--- FinalProject.py
+++ FinalProject.py
@@ -7,9 +7,6 @@ api = KaggleApi()
 api.authenticate()
 
 api.dataset_download_file('gpreda/covid-world-vaccination-progress',
-                          file_name='country_vaccinations_by_manufacturer.csv')
-
-api.dataset_download_file('gpreda/covid-world-vaccination-progress',
                           file_name='country_vaccinations.csv')
 
 import zipfile
@@ -21,18 +18,15 @@ import pandas as pd
 import numpy as np
 
 country_vac = pd.read_csv("country_vaccinations.csv")
-country_vac_mfr = pd.read_csv('country_vaccinations_by_manufacturer.csv')
 
 print(country_vac.head())
-print(country_vac_mfr.head())
 print(country_vac.shape)
-print(country_vac_mfr.shape)
 print(country_vac.country.unique())
 
 #Sorting ascending
 print(country_vac.sort_values('date').head())
 
-#indexing and grouping
+#indexing and grouping data for Ireland
 print(country_vac.columns)
 ireland_vac = country_vac[country_vac['country'] == 'Ireland']
 print(ireland_vac)
@@ -43,6 +37,34 @@ print(missing_values_count)
 
 cleaned_ireland_vac = ireland_vac.fillna(method='bfill', axis=0).fillna(0)
 print(cleaned_ireland_vac.isnull().sum())
+
+#iterate through each row and select
+#'date' and 'people_vaccinated' column respectively
+print("People vaccinated in Ireland according to date:\n")
+for i in range(len(cleaned_ireland_vac)):
+    print(cleaned_ireland_vac.iloc[i, 2], cleaned_ireland_vac.iloc[i, 9])
+
+uk_vac = country_vac[country_vac['country'] == 'United Kingdom']
+print(uk_vac)
+print(uk_vac.columns)
+cleaned_uk_vac = uk_vac.fillna(method='bfill', axis=0).fillna(0)
+print(cleaned_uk_vac.isnull().sum())
+
+print("People vaccinated in UK according to data:\n")
+for i in range(len(cleaned_uk_vac)):
+    print(cleaned_uk_vac.iloc[i, 2], cleaned_uk_vac.iloc[i, 9])
+
+#merge dataframes for data from Ireland and UK
+ireland_uk_vac = pd.merge(cleaned_ireland_vac, cleaned_uk_vac, on='date', how ='outer')
+print(cleaned_ireland_vac)
+print(ireland_uk_vac)
+print(ireland_uk_vac.columns)
+
+print("Obtain data of vaccinated people per hundred in Ireland and UK")
+vac_per_hundred_irl_uk = ireland_uk_vac[['country_x', 'people_vaccinated_per_hundred_x',
+                                        'country_y','people_vaccinated_per_hundred_y']]
+print(vac_per_hundred_irl_uk)
+
 
 
 
